d463be063a389eda42999256adf423133323b0ba
Reyhan Maulana <144551736+Rerey155-del@users.noreply.github.com>
2025-03-13 13:00:44 +0700

    upgrade tambahan

--- resources/views/Pages/welcome.blade.php
+++ resources/views/Pages/welcome.blade.php
@@ -552,18 +552,7 @@
 
 <body style="background-image:url('Image/bg.png');">
     <!-- navbar -->
-    <header style="width: 100%; padding: 20px 189px; background: white; position:fixed; z-index: 1000;">
-        <div style="display: flex; justify-content: space-between; align-items: center; max-width: 1280px; margin: 0 auto;">
-            <div style="display: flex; align-items: center;">
-                <img src="Image/Logo.png" alt="Logo" style="width: 192px; height: 50px;">
-            </div>
-            <nav style="display: flex; gap: 80px;">
-                <a href="#" style="text-decoration: none; font-family: Poppins; font-weight: 600; font-size: 16px; color: #333333; line-height: 140%;">Layanan</a>
-                <a href="#" style="text-decoration: none; font-family: Poppins; font-weight: 600; font-size: 16px; color: #333333; line-height: 140%;">Keunggulan</a>
-                <a href="#" style="text-decoration: none; font-family: Poppins; font-weight: 600; font-size: 16px; color: #333333; line-height: 140%;">Alur Pemesanan</a>
-            </nav>
-        </div>
-    </header>
+    <x-navbar></x-navbar>
 
     <div style="padding-top: 100px;"></div>
 
