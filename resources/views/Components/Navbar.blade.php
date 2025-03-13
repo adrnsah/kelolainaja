83063b4e14d76e79cf9607980dab921909dee4d0
Reyhan Maulana <144551736+Rerey155-del@users.noreply.github.com>
2025-03-13 13:39:39 +0700

    oke

--- resources/views/Components/Navbar.blade.php
+++ resources/views/Components/Navbar.blade.php
@@ -1,12 +1,15 @@
-<header style="width: 100%; padding:20px 189px; font-family: Poppins; background: transparance ; position:fixed; z-index: 1000;">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+<header style="width: 100%; padding:20px 189px; font-family: Lato; background: transparance ; position:fixed; z-index: 1000;">
     <div style="display: flex; justify-content: space-between; align-items: center; max-width: 1280px; margin: 0 auto;">
         <div style="display: flex; align-items: center;">
             <img src="Image/Logo.png" alt="Logo" style="width: 192px; height: 50px;">
         </div>
         <nav style="display: flex; gap: 80px;">
-            <a href="#" style="text-decoration: none; font-family: Poppins; font-weight: 600; font-size: 16px; color: #333333; line-height: 140%;">Layanan</a>
-            <a href="#" style="text-decoration: none; font-family: Poppins; font-weight: 600; font-size: 16px; color: #333333; line-height: 140%;">Keunggulan</a>
-            <a href="#" style="text-decoration: none; font-family: Poppins; font-weight: 600; font-size: 16px; color: #333333; line-height: 140%;">Alur Pemesanan</a>
+            <a href="#" style="text-decoration: none; font-family: Lato; font-weight: 600; font-size: 16px; color: #333333; line-height: 140%;">Layanan</a>
+            <a href="#" style="text-decoration: none; font-family: Lato; font-weight: 600; font-size: 16px; color: #333333; line-height: 140%;">Keunggulan</a>
+            <a href="#" style="text-decoration: none; font-family: Lato; font-weight: 600; font-size: 16px; color: #333333; line-height: 140%;">Alur Pemesanan</a>
         </nav>
     </div>
 </header>
